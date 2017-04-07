b219b980f7858b0787b9109113d213009687ff41
NeedlesInPenis <needlesinpenis@rediffmail.com>
2017-04-07 23:50:25 +0530

    implemented stack data structure

---
+++ stack.cs
@@ -0,0 +1,88 @@
+/*
+07th of April 2017
+Basic implementation of stack using linked list
+ */
+
+ using System;
+ 
+ namespace adt
+ {
+     /*
+     // "ERROR: namespace `adt` already contains definition for Node."
+     class Node 
+     {
+         public Node   next = null;
+         public object data = null;
+     }
+     */
+
+     interface IcStack
+     {
+         bool isEmpty();
+         int getSize();
+         
+         void push(object data);
+         object pop();
+
+         void print();
+     }
+
+     class cStack: IcStack
+     {
+         protected Node head = null;
+         protected int  size = 0;
+
+         public int  getSize() { return size; }
+         public bool isEmpty() { return getSize() <= 0 ? true : false; }
+
+         public void push(object data)
+         {
+             Node node = new Node();
+             node.data = data;
+             node.next = null;
+
+             if(isEmpty())
+             {
+                // creating stack
+                head = node;    
+             }
+             else 
+             {
+                 // prepending
+                 node.next = head;
+                 head = node;
+             }
+
+             size++;
+         }
+
+         public object pop()
+         {
+             if(isEmpty())
+             {
+                 Console.WriteLine("[ERROR] pop(): Stack is empty");
+                 return null;
+             }
+
+             object o = head.data;
+             head = head.next;
+
+             size--;
+             return o;
+         }
+
+         public void print()
+         {
+             Node curr = head;
+             int index = 0;
+             while(curr != null)
+             {
+                Console.WriteLine("[{0}]=>{1}", index, curr.data);
+                index++;
+                curr = curr.next;
+             }
+             Console.WriteLine("Stack size = " + getSize());
+         }
+
+     }
+ }
