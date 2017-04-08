114884516987c020291eed7451454db2bcfd13a0
NeedlesInPenis <needlesinpenis@rediffmail.com>
2017-04-08 17:12:49 +0530

    Created new for Node class

---
+++ NodeClass.cs
@@ -0,0 +1,41 @@
+/*
+8th of April 2017
+Node class for different structures.
+Using chain-inheritance
+*/
+
+namespace adt 
+{
+    class NodeLinkedList
+    {
+        public Node   next = null;
+        public object data = null;
+    }
+
+    class NodeDoubleLinkedList : NodeLinkedList
+    {
+        public Node prev = null;
+    }
+
+    class NodeBinarySearchTree : NodeDoubleLinkedList
+    {
+        public Node right = null;
+        public Node left = null;
+        public int val = 0;
+        
+        public Node parent = null;
+    }
+
+    // final class
+     class Node : NodeBinarySearchTree {}
+
+// public Node   next = null;
+// public object data = null;
+// public Node   prev = null; // need this for double linked list
+
+// // for trees
+// public Node right = null;
+// public Node left  = null;
+// public int  val   = 0;
+
+}
